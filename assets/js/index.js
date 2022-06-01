c29b816e2a871561791cc79e891a7d5bf076ccc0
Kadika <dryan10101@gmail.com>
2022-06-01 14:06:35 -0700

    Working product, only needs final touches now

--- assets/js/index.js
+++ assets/js/index.js
@@ -110,7 +110,7 @@ function runUntilDone(response, tm) {
         return;
     }
     return;
-}
+};
 
 function addEng(tm) {
     var createEng = inquirer.prompt(engQs);
@@ -124,7 +124,7 @@ function addEng(tm) {
             runUntilDone(response, tm);
         });
     });
-}
+};
 
 function addInt(tm) {
     var createInt = inquirer.prompt(intQs);
@@ -138,7 +138,7 @@ function addInt(tm) {
             runUntilDone(response, tm);
         });
     });
-}
+};
 
 function done(tm) {
     const template = createTemplate(tm);
@@ -167,15 +167,85 @@ const html1 = `<!DOCTYPE html>
         <h1>My Team</h1>
     </div>
 
-    <div id="container">`
+    <div id="container">`;
 
 const html2 = `</div>
     
 </body>
-</html>`
+</html>`;
 
 function createTemplate(tm) {
-    return html1 + html2;
+    var myhtml = html1;
+    for (t = 0; t < tm.length; t++) {
+        if (tm[t] instanceof Manager) {
+            myhtml = myhtml.concat(createMgrCard(tm[t]));
+        };
+        if (tm[t] instanceof Engineer) {
+            myhtml = myhtml.concat(createEngCard(tm[t]));
+        };
+        if (tm[t] instanceof Intern) {
+            myhtml = myhtml.concat(createIntCard(tm[t]));
+        };
+    };
+    myhtml = myhtml.concat(html2);
+    return myhtml;
+};
+
+function createMgrCard(mgr) {
+    var mgrTemp = `
+    <div class="card">
+        <h2>${mgr.name}</h2>
+        <h3>
+            Manager
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup iconCustomKadika" viewBox="0 0 16 16">
+                <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8zM13 2H2v10.5A1.5 1.5 0 0 0 3.5 14h8a1.5 1.5 0 0 0 1.5-1.5V2z"/>
+            </svg>
+        </h3>
+        <div class="graySec">
+            <div class="whiteSec">ID: ${mgr.id}</div>
+            <div class="whiteSec">Email: <a href="mailto:${mgr.email}">${mgr.email}</a></div>
+            <div class="whiteSec">Office Number: ${mgr.office}</div>
+        </div>
+    </div>`
+    return mgrTemp;
+}
+
+function createEngCard(eng) {
+    var engTemp = `
+    <div class="card">
+        <h2>${eng.name}</h2>
+        <h3>
+            Engineer
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wrench iconCustomKadika" viewBox="0 0 16 16">
+                <path d="M.102 2.223A3.004 3.004 0 0 0 3.78 5.897l6.341 6.252A3.003 3.003 0 0 0 13 16a3 3 0 1 0-.851-5.878L5.897 3.781A3.004 3.004 0 0 0 2.223.1l2.141 2.142L4 4l-1.757.364L.102 2.223zm13.37 9.019.528.026.287.445.445.287.026.529L15 13l-.242.471-.026.529-.445.287-.287.445-.529.026L13 15l-.471-.242-.529-.026-.287-.445-.445-.287-.026-.529L11 13l.242-.471.026-.529.445-.287.287-.445.529-.026L13 11l.471.242z"/>
+            </svg>
+        </h3>
+        <div class="graySec">
+            <div class="whiteSec">ID: ${eng.id}</div>
+            <div class="whiteSec">Email: <a href="mailto:${eng.email}">${eng.email}</a></div>
+            <div class="whiteSec">Github: <a href="https://github.com/${eng.github}" target="_blank">${eng.github}</a></div>
+        </div>
+    </div>`
+    return engTemp;
+}
+
+function createIntCard(int) {
+    var intTemp = `
+    <div class="card">
+        <h2>${int.name}</h2>
+        <h3>
+            Intern
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil iconCustomKadika" viewBox="0 0 16 16">
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+            </svg>
+        </h3>
+        <div class="graySec">
+            <div class="whiteSec">ID: ${int.id}</div>
+            <div class="whiteSec">Email: <a href="mailto:${int.email}">${int.email}</a></div>
+            <div class="whiteSec">School: ${int.school}</div>
+        </div>
+    </div>`
+    return intTemp;
 }
 
 start();
